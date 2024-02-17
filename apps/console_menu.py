de7d71b5738f0e70e8901a0fb7fbcef14536a947
SageScroll18144 <ffss@cin.ufpe.br>
2024-02-17 19:56:03 -0300

    implementando esboço do boot

--- apps/console_menu.py
+++ apps/console_menu.py
@@ -1,11 +1,39 @@
 import pygame
+import cv2
 from aliens import main as Alien
 from stars import main as Stars
 from blit_blends import main as LeftGame
 from blend_fill import main as RightGame
 
 def main():
+    
+    # setup 
     pygame.init()
+
+    video = cv2.VideoCapture("video/boot.mp4")
+    success, video_image = video.read()
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    window = pygame.display.set_mode(video_image.shape[1::-1])
+    clock = pygame.time.Clock()
+
+    run = success
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        success, video_image = video.read()
+        if success:
+            video_surf = pygame.image.frombuffer(
+                video_image.tobytes(), video_image.shape[1::-1], "BGR")
+        else:
+            run = False
+        window.blit(video_surf, (0, 0))
+        pygame.display.flip()
+
+    # loop
 
     try:
         while True:
