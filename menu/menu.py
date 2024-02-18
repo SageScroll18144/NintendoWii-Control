84b8a1806564c6c9682632494061a18b5315e128
Lucas Monterazo <74742441+Monterazo@users.noreply.github.com>
2024-02-17 23:14:36 -0300

    (feat): menu

---
+++ menu/menu.py
@@ -0,0 +1,75 @@
+import pygame
+import pygame_menu
+
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+
+
+
+def set_difficulty(value):
+    # Do the job here !
+    pass
+
+def start_the_game():
+    # Do the job here !
+    pass
+  
+
+meu_tema = pygame_menu.themes.Theme(
+    background_color= 	(18,52,86),  # cor de fundo
+    title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_SIMPLE,  # estilo da barra de título
+    title_background_color='white',
+    title_font=pygame_menu.font.FONT_FRANCHISE,  # fonte do título
+    title_font_size=60,  # tamanho da fonte do título
+    title_font_color='black',  # cor da fonte do título
+    widget_font=pygame_menu.font.FONT_FRANCHISE,  # fonte dos widgets
+    widget_font_size=40,  # tamanho da fonte dos widgets
+    widget_font_color='black',  # cor da fonte dos widgets
+    widget_alignment=pygame_menu.locals.ALIGN_CENTER,  # alinhamento dos widgets
+    widget_margin=(0, 20),  # margem dos widgets
+    
+)
+
+Doodle_Image = pygame_menu.baseimage.BaseImage(
+    image_path='./Doodle.png',
+)
+Dudu_Image = pygame_menu.baseimage.BaseImage(
+image_path='./DDtitle.png',
+)
+Agario_Image = pygame_menu.baseimage.BaseImage(
+image_path='./agario.jpg',
+)
+Flappy_Image = pygame_menu.baseimage.BaseImage(
+image_path='./flappybird.jpg',
+)
+Bomber_Image = pygame_menu.baseimage.BaseImage(
+image_path='./bomberman.jpg',
+)
+Space_Image = pygame_menu.baseimage.BaseImage(
+image_path='./SpaceInvaders.png',
+)
+Tetris_Image = pygame_menu.baseimage.BaseImage(
+image_path='./tetris.jpg',
+)
+
+
+menu = pygame_menu.Menu('                    ARCADE GAMES', 600, 400,
+                       theme= meu_tema)
+
+menu.add.vertical_margin(50)
+menu.add.button('                ', start_the_game(), background_color=Dudu_Image, font_size=80   )
+menu.add.button('                ', start_the_game(), background_color=Bomber_Image, font_size=80)
+menu.add.vertical_margin(20)
+menu.add.button('                ', start_the_game(),  background_color=Agario_Image, font_size=80 )
+menu.add.vertical_margin(20)
+menu.add.button('                ', start_the_game(), background_color=Flappy_Image, font_size=80)
+menu.add.vertical_margin(20)
+menu.add.button('                ', start_the_game(), background_color=Tetris_Image, font_size=80)
+menu.add.vertical_margin(20)
+menu.add.button('                ', start_the_game(), background_color=Space_Image, font_size=80)
+menu.add.vertical_margin(20)
+menu.add.button('Fechar', pygame_menu.events.EXIT)
+
+
+#Run the menu
+menu.mainloop(surface)
