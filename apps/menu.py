c44a55f51caa8aaf90a21ec0ce7f11e9fa18cf61
jdaniielc <jdsc2@cin.ufpe.br>
2024-02-20 01:39:28 -0300

    FIX: Remove run_menu to work

--- apps/menu.py
+++ apps/menu.py
@@ -4,96 +4,6 @@ import cv2
 
 from doom_app import run_doom
 
-def run_menu():
-    pygame.init()
-    surface = pygame.display.set_mode((600, 400))
-    create_menu(surface)
-
-def start_tetris():
-    print('tetris')
-
-def start_flappy():
-    print('flappy')
-
-def start_space():
-    print('space')
-
-def start_bomber():
-    print('bomber')
-
-def start_doodle():
-    print('doodle')
-
-def start_agario():
-    print('agario')
-
-def run_intro():
-    video = cv2.VideoCapture("./video/intro.mp4")
-    success, video_image = video.read()
-    fps = video.get(cv2.CAP_PROP_FPS)
-
-    window = pygame.display.set_mode(video_image.shape[1::-1])
-    clock = pygame.time.Clock()
-
-    run = success
-    while run:
-        clock.tick(fps)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        
-        success, video_image = video.read()
-        if success:
-            video_surf = pygame.image.frombuffer(
-                video_image.tobytes(), video_image.shape[1::-1], "BGR")
-        else:
-            run = False
-        window.blit(video_surf, (0, 0))
-        pygame.display.flip()
-
-def create_menu(surface: pygame.Surface): 
-    menu = pygame_menu.Menu(
-        '                    ARCADE GAMES',
-        600, 400, theme=main_theme, columns=2, rows=5
-    )
-
-    ## Column 1
-    menu.add.vertical_margin(10)
-    menu.add.button(
-        '1', start_doodle, background_color=base_images['doodle'],
-        **default_button_config
-    )
-    menu.add.button(
-        '2', start_bomber, background_color=base_images['bomber'],
-        **default_button_config
-    )
-    menu.add.button(
-        '3', start_agario, background_color=base_images['agario'],
-        **default_button_config
-    )
-    menu.add.button(
-        '4', run_doom, background_color=base_images['doom'],
-        **default_button_config
-    )
-
-    ## Column 2
-    menu.add.vertical_fill()
-    menu.add.button(
-        '5', start_flappy, background_color=base_images['flappy'],
-        **default_button_config
-    )
-    menu.add.button(
-        '6', start_tetris, background_color=base_images['tetris'],
-        **default_button_config
-    )
-    menu.add.button(
-        '7', start_space, background_color=base_images['space'],
-        **default_button_config
-    )
-    menu.add.button('Fechar', pygame_menu.events.EXIT)
-    #Run the menu
-    menu.mainloop(surface)
-
 # Menu variables
 main_theme = pygame_menu.themes.Theme(
     background_color= 	(18,52,86),
@@ -147,6 +57,92 @@ default_button_config = {
     'font_size': 80, 'padding': (10, 100), 'margin': (0, 10)
 }
 
-if __name__ == "_main_":
-    print('arrocha')
-    run_menu()
+def start_tetris():
+    print('tetris')
+
+def start_flappy():
+    print('flappy')
+
+def start_space():
+    print('space')
+
+def start_bomber():
+    print('bomber')
+
+def start_doodle():
+    print('doodle')
+
+def start_agario():
+    print('agario')
+
+def create_menu(surface: pygame.Surface): 
+    menu = pygame_menu.Menu(
+        '                    ARCADE GAMES',
+        600, 400, theme=main_theme, columns=2, rows=5
+    )
+
+    ## Column 1
+    menu.add.vertical_margin(10)
+    menu.add.button(
+        '1', start_doodle, background_color=base_images['doodle'],
+        **default_button_config
+    )
+    menu.add.button(
+        '2', start_bomber, background_color=base_images['bomber'],
+        **default_button_config
+    )
+    menu.add.button(
+        '3', start_agario, background_color=base_images['agario'],
+        **default_button_config
+    )
+    menu.add.button(
+        '4', run_doom, background_color=base_images['doom'],
+        **default_button_config
+    )
+
+    ## Column 2
+    menu.add.vertical_fill()
+    menu.add.button(
+        '5', start_flappy, background_color=base_images['flappy'],
+        **default_button_config
+    )
+    menu.add.button(
+        '6', start_tetris, background_color=base_images['tetris'],
+        **default_button_config
+    )
+    menu.add.button(
+        '7', start_space, background_color=base_images['space'],
+        **default_button_config
+    )
+    menu.add.button('Fechar', pygame_menu.events.EXIT)
+    #Run the menu
+    menu.mainloop(surface)
+
+def run_intro():
+    video = cv2.VideoCapture("./video/intro.mp4")
+    success, video_image = video.read()
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    window = pygame.display.set_mode(video_image.shape[1::-1])
+    clock = pygame.time.Clock()
+
+    run = success
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        success, video_image = video.read()
+        if success:
+            video_surf = pygame.image.frombuffer(
+                video_image.tobytes(), video_image.shape[1::-1], "BGR")
+        else:
+            run = False
+        window.blit(video_surf, (0, 0))
+        pygame.display.flip()
+
+run_intro()
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+create_menu(surface)
