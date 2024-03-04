b483de3fc6636380f393f6d3b02bbb9b141747b5
jdaniielc <jdsc2@cin.ufpe.br>
2024-03-04 11:16:49 -0300

    FIX: Exit doodle  for back to menu

--- apps/src/emulator_apps-1.1.0/src/doodle_app/main.py
+++ apps/src/emulator_apps-1.1.0/src/doodle_app/main.py
@@ -21,17 +21,19 @@ class Game():
         pygame.display.set_caption('Doodle jump')
     def event(self, event):
         if event.type == QUIT:
-            pygame.quit()
+            return False
         elif event.type == KEYUP:
             if event.key == K_ESCAPE:
                 if isinstance(self.location, GameLocation):
                     self.location = StartLocation(self)
                 elif isinstance(self.location, StartLocation):
-                    pygame.quit()
+                    return False
+        return True
                 
 
 # main function
 def run_doodle():
+    running = True
     game = Game()
 
     start_location = StartLocation(game)
@@ -40,16 +42,15 @@ def run_doodle():
     #game.location = GameLocation(game,'Т0стер')
 
     clock = pygame.time.Clock()
-    while 1:
+    while running:
         clock.tick(fps)
         game.location.draw()
         pygame.display.flip()
         event = []
         for event_ in pygame.event.get():
             event = event_
-            game.location.event(event)
-            game.event(event)     
-            
-    
+            exit1 = game.location.event(event)
+            exit2 = game.event(event)     
+            running = exit1 and exit2
 if __name__ == "__main__":
     run_doodle()
