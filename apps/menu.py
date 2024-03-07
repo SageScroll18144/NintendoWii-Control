89102d99119565f3e276f5bd97599c50521fe14a
jdaniielc <jdsc2@cin.ufpe.br>
2024-03-07 15:01:59 -0300

    REFACTOR: menu creation and game start functions

--- apps/menu.py
+++ apps/menu.py
@@ -10,7 +10,7 @@ from matris_app import run_matris
 from flappy_app import run_flappy
 from space_invaders_app import run_space_invaders
 
-# Menu variables
+
 main_theme = pygame_menu.themes.Theme(
     background_color= 	(18,52,86),
     title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_SIMPLE,
@@ -63,71 +63,74 @@ default_button_config = {
     'font_size': 80, 'padding': (10, 100), 'margin': (0, 10)
 }
 
-# Menu functions
-def start_game(game: str):
-    if game == 'doodle':
-        run_doodle()
-    elif game == 'agario':
-        run_agario()
-    elif game == 'doom':
-        run_doom()
-    elif game == 'flappy':
-        run_flappy()
-    elif game == 'space':
-        run_space_invaders()
-    elif game == 'tetris':
-        run_matris()
-    elif game == 'bomber':
-        run_bomberman()
-    else:
-        print('Game not found')
-    
-    pygame.display.set_mode((600, 500))
+class Menu:
+    def __init__(self):
+        self.surface = pygame.display.set_mode((600, 500))
+        self.create_menu()
 
-def create_menu(surface: pygame.Surface): 
-    menu = pygame_menu.Menu(
-        '                    ARCADE GAMES',
-        600, 500, theme=main_theme, columns=2, rows=5
-    )
+    def start_game(self, game: str):
+        if game == 'doodle':
+            run_doodle()
+        elif game == 'agario':
+            run_agario()
+        elif game == 'doom':
+            run_doom()
+        elif game == 'flappy':
+            run_flappy()
+        elif game == 'space':
+            run_space_invaders()
+        elif game == 'tetris':
+            run_matris()
+        elif game == 'bomber':
+            run_bomberman()
+        else:
+            print('Game not found')
+        
+        pygame.display.set_mode((600, 500))
 
-    ## Column 1
-    menu.add.vertical_margin(5)
-    menu.add.button(
-        '1',lambda: start_game('doodle'), background_color=base_images['doodle'],
-        **default_button_config
-    )
-    menu.add.button(
-        '2',lambda: start_game('bomber'), background_color=base_images['bomber'],
-        **default_button_config
-    )
-    menu.add.button(
-        '3',lambda: start_game('agario'), background_color=base_images['agario'],
-        **default_button_config
-    )
-    menu.add.button(
-        '4',lambda: start_game('doom'), background_color=base_images['doom'],
-        **default_button_config
-    )
+    def create_menu(self): 
+        menu = pygame_menu.Menu(
+            '                    ARCADE GAMES',
+            600, 500, theme=main_theme, columns=2, rows=5
+        )
 
-    ## Column 2
-    menu.add.vertical_margin(5)
-    menu.add.button(
-        '5',lambda: start_game('flappy'), background_color=base_images['flappy'],
-        **default_button_config
-    )
-    menu.add.button(
-        '6',lambda: start_game('tetris'), background_color=base_images['tetris'],
-        **default_button_config
-    )
-    menu.add.button(
-        '7',lambda: start_game('space'), background_color=base_images['space'],
-        **default_button_config
-    )
-    menu.add.button('Fechar', pygame_menu.events.EXIT)
-    #Run the menu
-    menu.mainloop(surface)
+        ## Column 1
+        menu.add.vertical_margin(5)
+        menu.add.button(
+            '1',lambda: self.start_game('doodle'), background_color=base_images['doodle'],
+            **default_button_config
+        )
+        menu.add.button(
+            '2',lambda: self.start_game('bomber'), background_color=base_images['bomber'],
+            **default_button_config
+        )
+        menu.add.button(
+            '3',lambda: self.start_game('agario'), background_color=base_images['agario'],
+            **default_button_config
+        )
+        menu.add.button(
+            '4',lambda: self.start_game('doom'), background_color=base_images['doom'],
+            **default_button_config
+        )
 
+        ## Column 2
+        menu.add.vertical_margin(5)
+        menu.add.button(
+            '5',lambda: self.start_game('flappy'), background_color=base_images['flappy'],
+            **default_button_config
+        )
+        menu.add.button(
+            '6',lambda: self.start_game('tetris'), background_color=base_images['tetris'],
+            **default_button_config
+        )
+        menu.add.button(
+            '7',lambda: self.start_game('space'), background_color=base_images['space'],
+            **default_button_config
+        )
+        menu.add.button('Fechar', pygame_menu.events.EXIT)
+        #Run the menu
+        menu.mainloop(self.surface) 
 
-pygame.init()
-surface = pygame.display.set_mode((600, 500))
-create_menu(surface)
+if __name__ == '__main__':
+    pygame.init()
+    menu = Menu()
